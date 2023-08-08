a4338c40754db2872265201a97343bc5361cc4da
Anirban <anirban2chakrabarti@gmail.com>
2023-08-08 22:48:38 +0530

    aug 22:48

--- fblkdedup.cpp
+++ fblkdedup.cpp
@@ -4,6 +4,8 @@
 #include <cmath>
 #include <iostream>
 #include <fstream> 
+#include <sstream>
+#include <iomanip> 
 #include<cmath>
 #include <vector>
 #include <ezpwd/rs>
@@ -35,6 +37,7 @@ int main(int argc, char** argv)
     vector<uint8_t> inputdata; 
     vector<uint8_t> subsample;
     vector<uint8_t> offsetdata;
+    vector<uint8_t> chunkdata;
     vector<int> intoffset;
     bool rowfound=false;
     char c;
@@ -43,11 +46,12 @@ int main(int argc, char** argv)
     uint8_t *cpakey=(unsigned char *)"dsMLtHNGAiNgRTwY"; 
     uint8_t *cpaiv=new uint8_t[BLOCK_SIZE];
     uint8_t *mlekey;
+    uint8_t *chunktag;
     unsigned char *cpacipher;
     unsigned char *final_cipher;
-    int CHUNK_SIZE = 255;
+    int CHUNK_SIZE = atoi(argv[2]);
     int BYTE_SIZE_LOC=ceil(ceil(log2(CHUNK_SIZE))/8);
-    int MAX_OFF_LEN=ECC_ERR_LMT*(BYTE_SIZE_LOC+1);
+    //int MAX_OFF_LEN=ECC_ERR_LMT*(BYTE_SIZE_LOC+1);
     auto st_start = std::chrono::high_resolution_clock::now();
 
     //---------------------------------------------------------------------   
@@ -72,14 +76,7 @@ int main(int argc, char** argv)
         cerr << "Could not open input file for reading!\n";
         return -1;
     }
-    
-    // offset file
-    std::string offfname=offdir+std::string(argv[1]);
-    ofstream offsetfile(offfname, ios::out | ios::trunc);
-    if (!offsetfile) {
-        cerr << "Could not open file for writing!\n";
-        return -1;
-    }  
+     
     // store mlekey file
     std::string keyfname=keydir+std::string(argv[1]);
     ofstream stormlekey(keyfname, ios::out | ios::trunc);
@@ -87,14 +84,13 @@ int main(int argc, char** argv)
         cerr << "Could not open file for writing!\n";
         return -1;
     } 
-    std::string mapfname=mapdir+std::string(argv[2]);
+    std::string mapfname=mapdir+std::string(argv[1]);
     std::ofstream mapfile(mapfname);
 
 
     //---------------------------------------------------------------------   
     //                   INPUT READ
     //---------------------------------------------------------------------   
-
     // Re read the input file for deduplication
     while (true) {
         c = infile.get();
@@ -102,8 +98,7 @@ int main(int argc, char** argv)
             break;
         inputdata.push_back(c);
     }
-    infile.clear();   //  Since ignore will have set eof.
-	infile.seekg( 0, std::ios_base::beg ); 
+    infile.close();
     msg_len=inputdata.size();
     
 
@@ -115,10 +110,9 @@ int main(int argc, char** argv)
     // subsampling is used to generate the label
     int data_size=0,chunk_cnt=0,data_remain=msg_len,inp_data_ptr=0;
     while(data_remain > 0)
-    {            
-            
-        cerr << " reading block \n";
-        
+    {      
+        chunk_cnt++;                   
+        cerr << " reading block \n";        
         if (data_remain >= CHUNK_SIZE)        
             data_size=CHUNK_SIZE;
         else
@@ -131,109 +125,138 @@ int main(int argc, char** argv)
         //---------------------------------------------------------------------   
         //                   LABEL GENERATION - FIXED SUBSAMPLING
         //---------------------------------------------------------------------     
-        // Create fingerprint of the file using subsampling. Fixed subsample used. Location of subsample is in parfile   
+        // Create fingerprint of the file using subsampling.
+        // Fixed subsample used. Location of subsample is in parfile   
         int index = 0;
-        while ( index < locatn.size() && (!infile.eof())) {
+        while ( index < locatn.size()) {
             loc = locatn.at(index);
-            infile.seekg(loc, ios::beg);
-            subsample.push_back(infile.get());
+            if (loc <= chunkdata.size())
+                subsample.push_back(chunkdata[loc]);
             index++;
         }
-        infile.clear();   
-	    infile.seekg( 0, std::ios_base::beg );
-
-
-            checkSimilarity((unsigned char *)&subsample[0],(int)subsample.size(),&rowfound,paritydata);        
-            if (rowfound ==true)
-            {
-                //---------------------------------------------------------------------   
-                //                   SUBSEQUENT ENCRYPTION
-                //---------------------------------------------------------------------  
-                //cerr << "Parity found: "<<rowfound<<endl;
-                //---------------------------------------------------------------------   
-                //                   BASE GENERATION
-                //---------------------------------------------------------------------  
-                //Run decode algorithm to deduplicate the file
-                reconst(inputdata,paritydata,recovdata,intoffset);     
-                //cerr << "Reconstruct Size: " <<recovdata.size()<<endl;
-                //---------------------------------------------------------------------   
-                //                   OFFSET GENERATION
-                //--------------------------------------------------------------------- 
-                int offsize=intoffset.size();
-                int ix=0;
-                // read offset for block by block
-                // read offset block by block, block length is EC parameter - message length   
-                while( ix < offsize){
-                    // number of flip/error symbol of a block bound by EC parameter - correction capability        
-                    int noerr=intoffset[ix++];
-                    // read all offset one by one, offset is position and flip symbol         
-                    for(auto k=0;k<noerr;k++){
-                        //get the position
-                        int loc=intoffset[ix++];        
-                        //convert the position from int to byte
-                        char cbyte[BYTE_SIZE_LOC];
-                        intToCharArray(cbyte, loc,BYTE_SIZE_LOC);                    
-                        for(int i=0;i<BYTE_SIZE_LOC;i++){
-                            offsetdata.push_back(cbyte[i]);
-                        }
-                        //get the flip symbol
-                        offsetdata.push_back(inputdata[loc]);
-                    }                                           
-                }
-                // PAD THE OFFSET
-                int OFF_PAD_SIZE=MAX_OFF_LEN - offsetdata.size();
-                for(auto ss=0;ss<OFF_PAD_SIZE;ss++)    
-                    offsetdata.push_back(0x00);
-                //---------------------------------------------------------------------   
-                //                   BASE ENCRYPTION
-                //---------------------------------------------------------------------         
-                // encrypt the base file , get from decode
-                mlekey = mleKeygen(recovdata);
-                //initialize MLE ciphertext lenth   
-                unsigned char *mlecipher=new unsigned char[msg_len+BLOCK_SIZE];
-                mleEncrypt(mlekey,(unsigned char *)&recovdata[0],mlecipher,ciph_len,msg_len);                                
-
-                //code file 
-                std::string cxtfname=cxtdir+std::string(argv[1]);
-                ofstream outfile(cxtfname, ios::out | ios::trunc);
-                if (!outfile) {
-                    cerr << "Could not open file for writing!\n";
-                    return -1;
-                }
-                outfile.write((char *)mlecipher,ciph_len);
-            }
-            else
-            {
-                //---------------------------------------------------------------------   
-                //                   INITIAL ENCRYPTION
-                //---------------------------------------------------------------------  
-                //---------------------------------------------------------------------   
-                //                   OFFSET GENERATION
-                //--------------------------------------------------------------------- 
-                // fill zero in offset file
-                for(auto ss=0;ss<MAX_OFF_LEN;ss++)    
-                    offsetdata.push_back(0x00);
-                //---------------------------------------------------------------------   
-                //                   BASE ENCRYPTION
-                //--------------------------------------------------------------------- 
-                //initial encryption - when no parity exists for fingerprint
-                //generate parity
-                genparity(inputdata,paritydata);    
-                // update the parity in database
-                insertParity((unsigned char *)&subsample[0],(int)subsample.size(), (unsigned char *)&paritydata[0],(int)paritydata.size());
-                //MLE Encrypt the input file  which is base file 
-                mlekey = mleKeygen(inputdata);
-                //initialize MLE ciphertext lenth
-                unsigned char *mlecipher=new unsigned char[msg_len+BLOCK_SIZE];
-                mleEncrypt(mlekey,(unsigned char *)&inputdata[0],mlecipher,ciph_len,msg_len);
-                outfile.write((char *)mlecipher,ciph_len);
-                
-            }
-            // CPA encrypt the offset file 
+        checkSimilarity((unsigned char *)&subsample[0],(int)subsample.size(),&rowfound,paritydata);        
+        if (rowfound ==true)
+        {
             //---------------------------------------------------------------------   
-            //                   OFFSET ENCRYPTION
+            //                   SUBSEQUENT ENCRYPTION
+            //---------------------------------------------------------------------  
+            //cerr << "Parity found: "<<rowfound<<endl;
+            //---------------------------------------------------------------------   
+            //                   BASE GENERATION
+            //---------------------------------------------------------------------  
+            //Run decode algorithm to deduplicate the file
+            reconst(chunkdata,paritydata,recovdata,intoffset);     
+            //cerr << "Reconstruct Size: " <<recovdata.size()<<endl;
+            //---------------------------------------------------------------------   
+            //                   OFFSET GENERATION
             //--------------------------------------------------------------------- 
-            cpa_msg_len=offsetdata.size();
+            int offsize=intoffset.size();
+            int ix=0;
+            // read offset for block by block
+            // read offset block by block, block length is EC parameter - message length   
+            while( ix < offsize){
+                // number of flip/error symbol of a block bound by EC parameter - correction capability        
+                int noerr=intoffset[ix++];
+                // read all offset one by one, offset is position and flip symbol         
+                for(auto k=0;k<noerr;k++){
+                    //get the position
+                    int loc=intoffset[ix++];        
+                    //convert the position from int to byte
+                    char cbyte[BYTE_SIZE_LOC];
+                    intToCharArray(cbyte, loc,BYTE_SIZE_LOC);                    
+                    for(int i=0;i<BYTE_SIZE_LOC;i++){
+                        offsetdata.push_back(cbyte[i]);
+                    }
+                    //get the flip symbol
+                    offsetdata.push_back(inputdata[loc]);
+                }                                           
+            }
+            // PAD THE OFFSET
+            //int OFF_PAD_SIZE=MAX_OFF_LEN - offsetdata.size();
+            //for(auto ss=0;ss<OFF_PAD_SIZE;ss++)    
+            //    offsetdata.push_back(0x00);
+            //---------------------------------------------------------------------   
+            //                   BASE ENCRYPTION
+            //---------------------------------------------------------------------         
+            // encrypt the base file , get from decode
+            mlekey = mleKeygen(recovdata);
+            //initialize MLE ciphertext lenth   
+            unsigned char *mlecipher=new unsigned char[data_size+BLOCK_SIZE];
+            mleEncrypt(mlekey,(unsigned char *)&recovdata[0],mlecipher,ciph_len,data_size);                                
+            //code file 
+            vector<uint8_t> Vmleciph(mlecipher, mlecipher + ciph_len);
+            chunktag = mleKeygen(Vmleciph);    
+            std::stringstream oss; 
+            for ( int j = 0; j < HKEY_SIZE; j++ )
+            {   
+                oss << std::setfill('0') << std::setw(2) << std::hex << static_cast< int >(chunktag[j]);
+            } 
+            mapfile << oss.str();
+            std::string cxtfname=cxtdir+oss.str();  
+            ofstream cxtfile(cxtfname, ios::out | ios::trunc);
+            if (!cxtfile) {
+                cerr << "Could not open file for writing!\n";
+                return -1;
+            }
+            cxtfile.write((char *)mlecipher,ciph_len);
+            cxtfile.close();
+        }
+        else
+        {
+            //---------------------------------------------------------------------   
+            //                   INITIAL ENCRYPTION
+            //---------------------------------------------------------------------  
+            //---------------------------------------------------------------------   
+            //                   OFFSET GENERATION
+            //--------------------------------------------------------------------- 
+            // fill zero in offset file
+            //for(auto ss=0;ss<MAX_OFF_LEN;ss++)    
+            //  offsetdata.push_back(0x00);
+            //---------------------------------------------------------------------   
+            //                   BASE ENCRYPTION
+            //--------------------------------------------------------------------- 
+            //initial encryption - when no parity exists for fingerprint
+            //generate parity
+            genparity(chunkdata,paritydata);    
+            // update the parity in database
+            insertParity((unsigned char *)&subsample[0],(int)subsample.size(), (unsigned char *)&paritydata[0],(int)paritydata.size());
+            //MLE Encrypt the input file  which is base file 
+            mlekey = mleKeygen(chunkdata);
+            //initialize MLE ciphertext lenth
+            unsigned char *mlecipher=new unsigned char[data_size+BLOCK_SIZE];
+            mleEncrypt(mlekey,(unsigned char *)&inputdata[0],mlecipher,ciph_len,data_size);
+            vector<uint8_t> Vmleciph(mlecipher, mlecipher + ciph_len);
+            chunktag = mleKeygen(Vmleciph);    
+            std::stringstream oss; 
+            for ( int j = 0; j < HKEY_SIZE; j++ )
+            {   
+                oss << std::setfill('0') << std::setw(2) << std::hex << static_cast< int >(chunktag[j]);
+            } 
+            mapfile << oss.str();
+            std::string cxtfname=cxtdir+oss.str();  
+            ofstream cxtfile(cxtfname, ios::out | ios::trunc);
+            if (!cxtfile) {
+                cerr << "Could not open file for writing!\n";
+                return -1;
+            }
+            cxtfile.write((char *)mlecipher,ciph_len);
+            cxtfile.close();
+
+        }
+        // CPA encrypt the offset file 
+        //---------------------------------------------------------------------   
+        //                   CHUNK OFFSET ENCRYPTION
+        //--------------------------------------------------------------------- 
+        std::string chunk_num=to_string(chunk_cnt);
+        std::string offfname=offdir+std::string(argv[1])+chunk_num;
+        ofstream offsetfile(offfname, ios::out | ios::trunc);
+        if (!offsetfile) {
+            cerr << "Could not open file for writing!\n";
+            return -1;
+        }         
+        cpa_msg_len=offsetdata.size();
+        if(cpa_msg_len > 0)
+        {
             cpacipher=new unsigned char[cpa_msg_len+BLOCK_SIZE];  
             cpaEncrypt(cpakey,cpaiv,(unsigned char *)&offsetdata[0],cpacipher,cpa_ciph_len,cpa_msg_len);  
             // cancatenate cpaiv cpacipher  == final cipher
@@ -241,28 +264,37 @@ int main(int argc, char** argv)
             std::copy(cpaiv, cpaiv+BLOCK_SIZE, final_cipher);
             std::copy(cpacipher, cpacipher+cpa_ciph_len, final_cipher +BLOCK_SIZE);
             // write offset file
+            // offset file        
             offsetfile.write((char *)final_cipher,BLOCK_SIZE + cpa_ciph_len);
-            //cpa encrypt mle key
-            //---------------------------------------------------------------------   
-            //                   KEY ENCRYPTION
-            //--------------------------------------------------------------------- 
-            cpa_msg_len=KEY_SIZE;
-            cpacipher=new unsigned char[cpa_msg_len+BLOCK_SIZE];  
-            cpaEncrypt(cpakey,cpaiv,mlekey,cpacipher,cpa_ciph_len,cpa_msg_len); 
-            // cancatenate cpaiv cpacipher  == final cipher
-            final_cipher = new unsigned char [BLOCK_SIZE + cpa_ciph_len];
-            std::copy(cpaiv, cpaiv+BLOCK_SIZE, final_cipher);
-            std::copy(cpacipher, cpacipher+cpa_ciph_len, final_cipher + BLOCK_SIZE);
-            stormlekey.write((char *)final_cipher,BLOCK_SIZE + cpa_ciph_len);   
-            //---------------------------------------------------------------------   
-            //                    IO CLOSE
-            //---------------------------------------------------------------------               
-            outfile.close();
-            offsetfile.close();
-        }
-        infile.close();
-        stormlekey.close();
-        auto st_end = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double, std::milli> st_float_ms = st_end - st_start;
-        std::cout << "Total  elapsed time is " <<  st_float_ms.count() << " milliseconds" << std::endl;
-    }    
+        }    
+        offsetfile.close();   
+        //cpa encrypt mle key
+        //---------------------------------------------------------------------   
+        //                  CHUNK KEY ENCRYPTION
+        //--------------------------------------------------------------------- 
+        cpa_msg_len=KEY_SIZE;
+        cpacipher=new unsigned char[cpa_msg_len+BLOCK_SIZE];  
+        cpaEncrypt(cpakey,cpaiv,mlekey,cpacipher,cpa_ciph_len,cpa_msg_len); 
+        // cancatenate cpaiv cpacipher  == final cipher
+        final_cipher = new unsigned char [BLOCK_SIZE + cpa_ciph_len];
+        std::copy(cpaiv, cpaiv+BLOCK_SIZE, final_cipher);
+        std::copy(cpacipher, cpacipher+cpa_ciph_len, final_cipher + BLOCK_SIZE);
+        stormlekey.write((char *)final_cipher,BLOCK_SIZE + cpa_ciph_len);   
+        //---------------------------------------------------------------------   
+        //                    CHUNK PROCESS END
+        //---------------------------------------------------------------------               
+        data_remain=data_remain-data_size;
+        chunkdata.resize(0);
+        subsample.resize(0);
+        offsetdata.resize(0);    
+    }
+    //---------------------------------------------------------------------   
+    //                    IO CLOSE
+    //---------------------------------------------------------------------                       
+ 
+    stormlekey.close();
+    mapfile.close();
+    auto st_end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> st_float_ms = st_end - st_start;
+    std::cout << "Total  elapsed time is " <<  st_float_ms.count() << " milliseconds" << std::endl;
+}    
