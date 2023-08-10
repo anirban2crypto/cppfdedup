03cfdd5df51ced0fe0525d42458b5f188d531e10
Anirban <anirban2chakrabarti@gmail.com>
2023-08-10 20:03:01 +0530

    aug 10 20:02 pm

--- manupulate_pixel.py
+++ manupulate_pixel.py
@@ -9,17 +9,17 @@ pixel_map = input_image.load()
   
 # Extracting the width and height 
 width, height = input_image.size
-
+print("Mode",input_image.mode)
+print("Widhth ",width,"height ",height)
 # getting the RGB pixel value.
-for i in range(int(width)):
-    r, g, b, p = input_image.getpixel((i, int(height-1)))
-       
-    # Apply formula of grayscale:
-    #grayscale = (0.299*r + 0.587*g + 0.114*b)
-    grayscale = (1*r + 0*g + 0*b)
-
-    # setting the pixel value.
-    pixel_map[i, int(height-1)] = (int(grayscale), int(grayscale), int(grayscale))
-  
+for i in range(height):
+        print(i)    
+        row=[ input_image.getpixel((i, j)) for j in range(width)]
+        if(sum(row) !=0 ):
+            a=5
 # Saving the final output
-input_image.save("grayscale", format="bmp")
+input_image.putpixel((38, 100), 250)
+file_out = f1_name[:-4]+"_modify.bmp"
+print(file_out)
+input_image.save(file_out)
+
