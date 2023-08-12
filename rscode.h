45e379ae149cbcb635893bc84ecd5aebd893877f
Anirban <anirban2chakrabarti@gmail.com>
2023-08-12 20:19:42 +0530

    abcd

--- rscode.h
+++ rscode.h
@@ -8,8 +8,8 @@
 #include <ezpwd/rs>
 #include <vector> 
 #include <chrono>
-#define ECC_ERR_LMT 50
-#define ECC_BLK_LEN 1947
+#define ECC_ERR_LMT 25
+#define ECC_BLK_LEN 205
 using namespace std; 
 void genparity(vector<uint8_t> &msgdata,vector<uint8_t> &paritydata);
 void reconst(vector<uint8_t> &erronsdata,vector<uint8_t> &paritydata,vector<uint8_t> &recovdata,vector<int> &intoffset);
